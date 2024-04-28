4d93b86da1367096f170cbec048802d970d377c7
Sven Latzel <sven.latzel@vitale-arbeitskultur.de>
2024-04-28 16:53:06 +0200

    added styling to the list

--- lib/widgets/home_body.dart
+++ lib/widgets/home_body.dart
@@ -3,7 +3,12 @@ import 'package:flutter/material.dart';
 class HomeBody extends StatelessWidget {
   HomeBody({super.key});
 
-  final List<String> items = ['Daten 1', 'Daten 2', 'Daten 3', 'Daten 4'];
+  final List<String> items = [
+    'Element 1',
+    'Element 2',
+    'Element 3',
+    'Element 4'
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +20,15 @@ class HomeBody extends StatelessWidget {
           itemBuilder: (context, index) {
             return ListTile(
               title: Text(items[index]),
+              subtitle: const Text('This is a nice ling subtitle!'),
+              leading: const Icon(Icons.abc_sharp),
+              trailing: IconButton(
+                icon: const Icon(Icons.explicit),
+                onPressed: () {},
+              ),
+              isThreeLine: false,
+              dense: false,
+              visualDensity: VisualDensity(horizontal: -2, vertical: -2),
             );
           },
         ),
